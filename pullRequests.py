ca2790494d1ca01224e52ade5163c7bf0705c251
ericwallat <Michaelst04!>
2017-10-23 11:15:27 -0500

    methods added: get_repos get_pulls get_merge make_request

--- pullRequests.py
+++ pullRequests.py
@@ -1,27 +1,49 @@
 import requests
+import sys
+
+GITHUB_API = 'https://api.github.com/'
+TOKEN = str(sys.argv[1])
 
 
 class github(object):
     def __init__(self, org):
         self.org = org
-        self.url = 'https://api.github.com/orgs/' + str(self.org)
 
     def __str__(self):
         return str(self.org)
 
     def get_repos(self):
-        try:
-            url2 = self.url + '/repos/'
-            print url2
-            request = Request(self.url + '/repos/')
-            response = urlopen(request)
-            repos = response.read()
-            return repos
-        except URLError, e:
-            print 'Invalid organization. Error code:', e
+        url2 = GITHUB_API + 'orgs/' + str(self.org) + '/repos'
+        repos = self.make_request(url2)
+        return repos.json()
+
+    def get_pulls(self, name):
+        url2 = GITHUB_API + 'repos/' + self.org + '/' + name + '/pulls'
+        pulls = self.make_request(url2)
+        return pulls.json()
+
+    def get_merge(self, url):
+        r = self.make_request(url)
+        print(r.json())
+
+    def make_request(self, url):
+        response = requests.get(url, headers={'Authorization': 'token %s' % TOKEN})
+        return response
+
+
+def main():
+
+    g = github("lodash")
+    pulls = []
+    for repo in g.get_repos():
+        pull = g.get_pulls(repo['name'])
+        if pull:
+            pulls.append(pull)
+    pulls = [item for sublist in pulls for item in sublist]
+    for pull in pulls:
+        g.get_merge(pull['url'])
 
 
 if __name__ == '__main__':
 
-    org = github("lodash")
-    print org.get_repos()
+    main()
